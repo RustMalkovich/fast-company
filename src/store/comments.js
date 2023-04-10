22a46be307e3bccd8b78edbbaa5176e5c5e722da
Rustam Muratov <rustmalkovich55@gmail.com>
2023-04-10 12:33:05 +0300

    add redux in comments

---
+++ src/store/comments.js
@@ -0,0 +1,89 @@
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import commentService from "../services/comment.service";
+
+const commentsSlice = createSlice({
+    name: "comments",
+    initialState: {
+        entities: null,
+        isLoading: true,
+        error: null
+    },
+    reducers: {
+        commentsRequested: (state) => {
+            state.isLoading = true;
+        },
+        commentsReceived: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        commentsRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        commentsCreated: (state, action) => {
+            if (!Array.isArray(state.entities)) {
+                state.entities = [];
+            }
+            state.entities.push(action.payload);
+        },
+        commentsRemoved: (state, action) => {
+            state.entities = state.entities.filter(
+                (c) => c._id !== action.payload
+            );
+        }
+    }
+});
+
+const { reducer: commentsReducer, actions } = commentsSlice;
+const {
+    commentsRequested,
+    commentsReceived,
+    commentsRequestFailed,
+    commentsCreated,
+    commentsRemoved
+} = actions;
+
+const commentCreateRequested = createAction("comments/commentCreateRequested");
+const createCommentFailed = createAction("comments/createCommentFailed");
+const commentRemoveRequested = createAction("comments/commentRemoveRequested");
+const commentRemoveRequestFailed = createAction(
+    "comments/commentRemoveRequestFailed"
+);
+
+export const loadCommentsList = (userId) => async (dispatch) => {
+    dispatch(commentsRequested());
+    try {
+        const { content } = await commentService.getComments(userId);
+        dispatch(commentsReceived(content));
+    } catch (error) {
+        dispatch(commentsRequestFailed(error.message));
+    }
+};
+
+export const createComment = (comment) => async (dispatch) => {
+    dispatch(commentCreateRequested());
+    try {
+        const { content } = await commentService.createComment(comment);
+        dispatch(commentsCreated(content));
+    } catch (error) {
+        dispatch(createCommentFailed(error.message));
+    }
+};
+
+export const removeComment = (id) => async (dispatch) => {
+    dispatch(commentRemoveRequested());
+    try {
+        const { content } = await commentService.removeComment(id);
+        if (content === null) {
+            dispatch(commentsRemoved(id));
+        }
+    } catch (error) {
+        dispatch(commentRemoveRequestFailed(error.message));
+    }
+};
+
+export const getComments = () => (state) => state.comments.entities;
+export const getCommentsLoadingStatus = () => (state) =>
+    state.comments.isLoading;
+
+export default commentsReducer;
--- src/store/users.js
+++ src/store/users.js
@@ -4,6 +4,7 @@ import authService from "../services/auth.service";
 import localStorageService from "../services/localStorage.service";
 import getRandomInt from "../utils/getRandomInt";
 import history from "../utils/history";
+import { generateAuthError } from "../utils/generateAuthError";
 
 const initialState = localStorageService.getAccessToken()
     ? {
@@ -62,6 +63,9 @@ const usersSlice = createSlice({
             state.entities[
                 state.entities.findIndex((u) => u._id === action.payload._id)
             ] = action.payload;
+        },
+        authRequested(state) {
+            state.error = null;
         }
     }
 });
@@ -105,7 +109,13 @@ export const login =
             localStorageService.setTokens(data);
             history.push(redirect);
         } catch (error) {
-            dispatch(authRequestFailed(error.message));
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                const errorMessage = generateAuthError(message);
+                dispatch(authRequestFailed(errorMessage));
+            } else {
+                dispatch(authRequestFailed(error.message));
+            }
         }
     };
 
@@ -181,5 +191,5 @@ export const getCurrentUserData = () => (state) => {
         ? state.users.entities.find((u) => u._id === state.users.auth.userId)
         : null;
 };
-
+export const getAuthErrors = () => (state) => state.users.error;
 export default usersReducer;
